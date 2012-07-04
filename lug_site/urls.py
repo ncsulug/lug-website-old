d36f5f457638a5c43e5d40217f90e9214d63d795
Matthew Frazier <leafstormrush@gmail.com>
2012-07-04 15:22:48 -0400

    added styles, a layout, and a dummy index page
    I added a Bootstrap-based theme and stylesheet for the Web site,
    as well as the beginnings of a layout template and a placeholder
    index page.
    
    Note that the use of CC-BY-SA is not 100% final, we must have a
    LUG discussion in order to determine whether to add an -NC
    option or not.

--- lug_site/urls.py
+++ lug_site/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
