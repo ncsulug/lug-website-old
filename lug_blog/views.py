dba42fe31c79f8824d3f38922a03a9f59b7ceaef
Barry Peddycord III <ishara@isharacomix.org>
2013-01-25 17:38:36 -0500

    Adding RSS support to the blog.

--- lug_blog/views.py
+++ lug_blog/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 from django.views.generic import DetailView
 from django.views.generic.dates import (ArchiveIndexView, YearArchiveView,
                                         MonthArchiveView, DayArchiveView)
+from django.contrib.syndication.views import Feed
 from .models import BlogPost
+
 
 archive_options = dict(
     model = BlogPost,
@@ -22,3 +26,20 @@ day_archive = DayArchiveView.as_view(month_format='%m',
 post_detail = DetailView.as_view(model = BlogPost,
                                  queryset = BlogPost.objects.published(),
                                  context_object_name='post')
+
+
+
+class BlogFeed(Feed):
+    title = "LUG @ NC State"
+    link = "/feed/"
+    description = "Posts from the LUG at NC State University"
+
+    def items(self):
+        return BlogPost.objects.order_by('-pub_date')[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
+
